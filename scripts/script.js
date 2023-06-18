74fff64496f0ae2651646de9c8e6829ea63b14cc
rhonvyramos <rhonvyramos@gmail.com>
2023-06-18 14:24:27 -0400

    added real time to header; user can compare real time to daily schedule if needed

---
+++ scripts/script.js
@@ -0,0 +1,11 @@
+var headerTime = document.getElementById("time_realtime");
+
+// function to display real time for the user to compare their schedule to
+// code taken from: https://stackoverflow.com/questions/50438363/show-real-time-clock-on-html-using-javascript
+function refreshTime() {
+    var dateString = new Date().toLocaleString("en-US", {timeZone: "America/Sao_Paulo"});
+    var formattedString = dateString.replace(", ", " - ");
+    headerTime.innerHTML = formattedString;
+};
+  
+setInterval(refreshTime, 1000);
