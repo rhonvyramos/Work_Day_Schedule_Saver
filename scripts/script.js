18473652cab07b0c25ea0c24b8207895567ff998
rhonvyramos <rhonvyramos@gmail.com>
2023-06-18 14:45:19 -0400

    corrected time zone in time label; changed from San Paolo to New York

--- scripts/script.js
+++ scripts/script.js
@@ -3,7 +3,7 @@ var headerTime = document.getElementById("time_realtime");
 // function to display real time for the user to compare their schedule to
 // code taken from: https://stackoverflow.com/questions/50438363/show-real-time-clock-on-html-using-javascript
 function refreshTime() {
-    var dateString = new Date().toLocaleString("en-US", {timeZone: "America/Sao_Paulo"});
+    var dateString = new Date().toLocaleString("en-US", {timeZone: "America/New_York"});
     var formattedString = dateString.replace(", ", " - ");
     headerTime.innerHTML = formattedString;
 };
