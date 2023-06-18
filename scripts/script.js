042745e893485b536bf3f0ab35566b7cf4f3338c
rhonvyramos <rhonvyramos@gmail.com>
2023-06-18 15:13:25 -0400

    replaced timeslot for loop with html elements; easier to maintain; added font family

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,4 @@
-var headerTime = document.getElementById("time_realtime");
+let headerTime = document.getElementById("time_realtime");
 
 // function to display real time for the user to compare their schedule to
 // code taken from: https://stackoverflow.com/questions/50438363/show-real-time-clock-on-html-using-javascript
@@ -7,12 +7,4 @@ function refreshTime() {
     var formattedString = dateString.replace(", ", " - ");
     headerTime.innerHTML = formattedString;
 };
-  
 setInterval(refreshTime, 1000);
-
-// let timeslot = document.getElementById("schedule_timeslot").cloneNode(true);
-var dailySchedule = document.getElementById("content_daily_schedule");
-for(var x = 0; x < 8; x++) {
-    let timeslot = document.getElementById("schedule_timeslot").cloneNode(true);
-    $(dailySchedule).append(timeslot)
-};
